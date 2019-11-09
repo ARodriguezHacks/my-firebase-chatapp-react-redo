d82526561f4a2064155dc704ec40c0e7cff34d03
Angie Rodriguez <angrodri902@gmail.com>
2019-11-09 02:58:52 -0800

    Initiate new design for user log in

--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 
+//var firebaseui = require('firebaseui');
+
+//var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
 class User extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      signUp: false,
+      email: '',
+      password: ''
+    }
 
     this.signIn = this.signIn.bind(this);
     this.signOut = this.signOut.bind(this);
   }
 
+  startSignUp() {
+    const form = document.getElementById('signUp-form');
+    form.style.visibility = "visible";
+  }
+
   signIn() {
-    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    const provider = new this.props.firebase.auth.signInWithEmailAndPassword()
     this.props.firebase.auth().signInWithPopup( provider );
   }
 
   signOut() {
     this.props.firebase.auth().signOut();
+  }
+
+  handleEmail(email) {
+
+    this.setState({
+      email: email
+    });
+
+    const provider = new this.props.firebase.auth.createUserWithEmailAndPassword();
+    this.props.firebase.auth().signInWithPopup( provider );
+  }
+
+  handlePassword(password) {
+    this.setState({
+      password: password
+    });
   }
 
   handleChange(user) {
@@ -39,10 +69,22 @@ class User extends Component {
         <button onClick={this.signIn}
         onChange={(user) => this.handleChange(user)}>
         {(this.props.currentUser) === null ?
-          <span>Sign Up/Log In</span> :
+          <span>Log In</span> :
           <span>Signed In</span>
         }
         </button>
+        <button onClick={this.startSignUp}
+          onChange={() => this.handleSignUp()}>
+            Sign up
+        </button>
+        <form id="signUp-form">
+          <h4>Sign in to start chatting</h4>
+          <label><strong>Email</strong></label>
+          <input type="email" id="email" name="email" value={this.state.email} placeholder="Email" />
+          <label><strong>Password</strong></label>
+          <input type="password" id="password" name="password" value={this.state.password} placeholder="Password" />
+          <button type="submit" class="button">Log In</button>
+        </form>
         {(this.props.currentUser) !== null ? <button onClick={this.signOut}>Sign Out</button> : (null) }
       </section>
     );
